bc0d6fc7499564daca9f076fe8e1c759267ca971
xLPMG@LeChuck <xxlpmghdxx@gmail.com>
2024-04-04 19:18:33 +0200

    finished creation

---
+++ week_1/task-2-1-Creation-3.py
@@ -0,0 +1,38 @@
+import torch
+import numpy
+
+# declare dimensions
+zDim = 4
+yDim = 2
+xDim = 3
+
+tensorList = []
+# since the values are just consecutively increased numbers, 
+# we can use a counter variable
+i = 0
+
+# for each of the T_i,
+# add the list of rows
+for z in range(zDim):
+    # t_y contains yDim t_x-lists (rows)
+    t_y = []
+    for y in range(yDim):
+        # create a list for a new row 
+        # and fill it with xDim values
+        t_x = []
+        for x in range(xDim):
+            t_x.append(i)
+            i += 1
+        t_y.append(t_x)
+    tensorList.append(t_y)
+       
+array = numpy.array(tensorList)             
+        
+tensor = torch.tensor(array)
+
+print("ARRAY")
+print(array)
+print("TENSOR")
+print(tensor)
+
+# Observation: the numpy representation uses whitespaces instead of commata
