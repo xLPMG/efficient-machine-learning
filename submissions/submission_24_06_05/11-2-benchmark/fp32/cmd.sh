be5457b550e31d5f267fdd6ebeeff4d0a4bb37af
xLPMG@LeChuck <xxlpmghdxx@gmail.com>
2024-06-04 19:10:48 +0200

    add scripts for int8 benchmark

--- submissions/submission_24_06_05/11-2-benchmark/fp32/cmd.sh
+++
@@ -1,2 +0,0 @@
-export QNN_SDK_ROOT=/opt/qcom/aistack/qnn/2.18.0.240101/
-python ${QNN_SDK_ROOT}/benchmarks/QNN/qnn_bench.py -c fp32.json
---
+++ submissions/submission_24_06_05/11-2-benchmark/int8/runBenchmarkCPU.sh
@@ -0,0 +1,3 @@
+export QNN_SDK_ROOT=/opt/qcom/aistack/qnn/2.18.0.240101/
+export ANDROID_NDK_ROOT=/opt/qcom/HexagonSDK/5.5.0.1/tools/android-ndk-r25c
+python ${QNN_SDK_ROOT}/benchmarks/QNN/qnn_bench.py -c cpu-int8.json --dsp_type v79
